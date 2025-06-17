5bbd6c1dfc92b58219a171b2622f2a1b25db60ae
Farhad Rasoli <rasoolyfarhad7@gmail.com>
2025-06-17 01:16:34 -0400

    added all routes to be used, need to implement post requests and work on stylings

--- server.js
+++ server.js
@@ -12,10 +12,32 @@ app.set("view engine", "ejs");
 app.use(express.static(assetSource));
 app.use(express.urlencoded({extended : true}));
 
+const indexRouter = require("./routes/index/IndexRouter");
+const CategoryRouter = require("./routes/CategoryRouter");
+const MovieRouter = require("./routes/MovieRouter");
 
 
 
 
+
+
+//for homepage, includes the template categories
+
+app.use("/", indexRouter);
+
+
+
+
+//for 
+
+app.use("/category", CategoryRouter);
+
+//
+
+app.use("/movies", MovieRouter)
+
+
+//
 
 
 
