4f59b88fd27018a74b461117227b115c349e0733
Farhad Rasoli <rasoolyfarhad7@gmail.com>
2025-06-18 01:31:47 -0400

    added route for indexcontroller of movies

--- routes/MovieRouter.js
+++ routes/MovieRouter.js
@@ -4,10 +4,21 @@ const {Router} = require("express");
 
 const MovieRouter = Router();
 const MovieFormController = require("../controllers/MovieFormController");
+const AddMovieController = require("../controllers/AddMovieController");
 
+const MovieIndexController = require("../controllers/MovieIndexController");
+
+
+
+
+MovieRouter.get("/", MovieIndexController);
 
 
 MovieRouter.get("/create", MovieFormController);
+
+
+
+MovieRouter.post("/Add", AddMovieController);
 
 
 module.exports = MovieRouter;
