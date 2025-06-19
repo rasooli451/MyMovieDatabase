8d2c192eaad1d49e0bed34a2a9188d2ae7722e7d
Farhad Rasoli <rasoolyfarhad7@gmail.com>
2025-06-19 01:57:15 -0400

    added routes for editing movie, posting the edited movie and removing the movie

--- routes/MovieRouter.js
+++ routes/MovieRouter.js
@@ -7,8 +7,9 @@ const MovieFormController = require("../controllers/MovieFormController");
 const AddMovieController = require("../controllers/AddMovieController");
 
 const MovieIndexController = require("../controllers/MovieIndexController");
-
-
+const EditMovieController = require("../controllers/EditMovieController");
+const EditPostMovieController = require("../controllers/EditPostMovieController");
+const RemoveMovieController = require("../controllers/RemoveMovieController");
 
 
 MovieRouter.get("/", MovieIndexController);
@@ -20,5 +21,11 @@ MovieRouter.get("/create", MovieFormController);
 
 MovieRouter.post("/Add", AddMovieController);
 
+
+MovieRouter.get("/edit/:id", EditMovieController);
+
+MovieRouter.post("/edit/:id", EditPostMovieController);
+
+MovieRouter.get("/remove/:id", RemoveMovieController);
 
 module.exports = MovieRouter;
