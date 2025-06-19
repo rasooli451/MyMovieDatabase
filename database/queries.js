5cc91babe3418ffa1a818d2699b1f316abf5af90
Farhad Rasoli <rasoolyfarhad7@gmail.com>
2025-06-19 01:54:17 -0400

    added functions for editing a movie, for getting count of category, changed some functions that had bugs

--- database/queries.js
+++ database/queries.js
@@ -12,7 +12,7 @@ const asyncHandler = require("express-async-handler");
 const CreateCategory = asyncHandler(async function(name, description, data, filetype){
     const exists = await CategoryExists(name);
     if (!exists){
-        await Pool.query("INSERT INTO categories (name, description,picture,datatype) VALUES ($1,$2,$3,$4)", [name, description,data,filetype]);
+        await Pool.query("INSERT INTO categories (name, description,picture,datatype,count) VALUES ($1,$2,$3,$4,$5)", [name, description,data,filetype,0]);
         return true;
     }
     else{
@@ -34,12 +34,20 @@ const CategoryExists = asyncHandler(async function(name){
 
 
 const AddMovieToCategory = asyncHandler(async function(movieName, releaseDate, rating,categoryId, directorName, picture, datatype){
+    const movieExistence = await movieExists(movieName, categoryId);
+    if (!movieExistence){
     const exists = await DirectorExists(directorName);
     if (!exists){
         await AddDirector(directorName);
     }
     const director_id = await GetDirectorId(directorName);
     await Pool.query("INSERT INTO movies (Name, releaseDate, rating, category_id, director_id, picture, datatype) VALUES ($1,$2,$3,$4,$5,$6,$7)", [movieName, releaseDate, rating, categoryId, director_id,picture,datatype]);
+    await increaseCount(categoryId);
+    return true;
+    }
+    else{
+        return false;
+    }
 })
 
 
@@ -84,8 +92,32 @@ const GetDirectorId = asyncHandler(async function(directorName){
 
 
 
-const RemoveMovie = asyncHandler(async function(movieName){
-    await Pool.query("DELETE FROM movies WHERE Name=($1)", [movieName]);
+const RemoveMovie = asyncHandler(async function(movieId, category_id){
+
+    await Pool.query("DELETE FROM movies WHERE movie_id=($1)", [movieId]);
+    await decreaseCount(category_id);
+})
+
+
+
+
+const increaseCount = asyncHandler(async function(category_id){
+    const count = await getCount(category_id);
+    await Pool.query("UPDATE categories SET count=($1) WHERE category_id=($2)", [count + 1, category_id]);
+})
+
+
+const decreaseCount = asyncHandler(async function(category_id){
+    const count = await getCount(category_id);
+    await Pool.query("UPDATE categories SET count=($1) WHERE category_id=($2)", [count - 1, category_id]);
+})
+
+
+const getCount = asyncHandler(async function(category_id){
+    const {rows} = await Pool.query("SELECT count FROM categories WHERE category_id=($1)", [category_id]);
+    console.log(category_id);
+    console.log(rows);
+    return rows[0].count;
 })
 
 
@@ -101,10 +133,45 @@ const getAllCategories = asyncHandler(async function(){
 
 
 const getAllMovies = asyncHandler(async function(){
-    const {rows} = await Pool.query("SELECT * FROM movies");
+    const {rows} = await Pool.query("SELECT movies.*, categories.name AS category, directors.name AS director FROM movies INNER JOIN categories ON movies.category_id=categories.category_id INNER JOIN directors ON movies.director_id=directors.director_id");
     return rows;
+})
+
+const movieExists = asyncHandler(async function(movieName, category_id){
+    const {rows} = await Pool.query("SELECT * FROM movies WHERE Name ILIKE ($1) AND category_id=($2)", [movieName, category_id]);
+    if (rows.length > 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+})
+
+
+const getMovie = asyncHandler(async function(movieId){
+    const {rows} = await Pool.query("SELECT movies.*, categories.name AS category, directors.name AS director FROM movies INNER JOIN categories ON movies.category_id=categories.category_id INNER JOIN directors ON movies.director_id=directors.director_id WHERE movies.movie_id=($1)", [movieId]);
+    return rows[0];
 })
 
 
 
-module.exports = {CreateCategory, CategoryExists, AddMovieToCategory, GetCategoryId, AddDirector, DirectorExists, RemoveMovie, RemoveCategory, getAllCategories, getAllMovies};
+const editMovie = asyncHandler(async function(movieId,movieName, releaseDate, rating,categoryId, directorName, picture, datatype){
+    const exists = await DirectorExists(directorName);
+    if (!exists){
+        await AddDirector(directorName);
+    }
+    const director_id = await GetDirectorId(directorName);
+    let categoryChanged = false;
+    const {rows} = await Pool.query("SELECT category_id FROM movies WHERE movie_id=($1)", [movieId]);
+    if (rows[0].category_id != categoryId){
+        categoryChanged = true;
+    }
+    await Pool.query("UPDATE movies SET name=($1), releasedate=($2), rating=($3),category_id=($4),director_id=($5),picture=($6),datatype=($7) WHERE movie_id=($8)", [movieName, releaseDate, rating, categoryId, director_id, picture, datatype, movieId]);
+    if (categoryChanged){
+        await increaseCount(categoryId);
+        await decreaseCount(rows[0].category_id);
+    }
+})
+
+
+module.exports = {CreateCategory, CategoryExists, AddMovieToCategory, GetCategoryId, AddDirector, DirectorExists, RemoveMovie, RemoveCategory, getAllCategories, getAllMovies,getMovie, editMovie};
