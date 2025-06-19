79ab5d9daaee2f8fb18969701ceff15898f50778
Farhad Rasoli <rasoolyfarhad7@gmail.com>
2025-06-19 01:52:37 -0400

    the controller to handle the remove movie button

---
+++ controllers/RemoveMovieController.js
@@ -0,0 +1,22 @@
+
+const {RemoveMovie} = require("../database/queries");
+
+
+
+
+
+
+const RemoveMovieController = async function(req, res){
+    const {id} = req.params;
+    const movieId = id.split(",")[0];
+    const categoryId = id.split(",")[1];
+    await RemoveMovie(movieId, categoryId);
+    if (req.url.includes("movies"))
+        res.redirect("/movies");
+    else
+        res.redirect("/");
+}
+
+
+
+module.exports = RemoveMovieController;
