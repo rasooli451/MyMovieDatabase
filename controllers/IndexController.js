8804b8507806badbd0c33c07af78239935a09ade
Farhad Rasoli <rasoolyfarhad7@gmail.com>
2025-06-20 02:13:19 -0400

    added a path property to help with redirection process

--- controllers/IndexController.js
+++ controllers/IndexController.js
@@ -5,7 +5,7 @@ const asyncHandler = require("express-async-handler");
 const IndexController = asyncHandler(async function(req, res){
     const categories = await getAllCategories();
     const movies = await getAllMovies();
-    res.render("Pages/index", {content : "../Partials/homePage", categories, movies});
+    res.render("Pages/index", {content : "../Partials/homePage", categories, movies, path : ""});
 })
 
 
