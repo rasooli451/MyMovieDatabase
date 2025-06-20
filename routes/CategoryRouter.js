4e0de1d1fd140be07f5c3b613386022b9386ebb3
Farhad Rasoli <rasoolyfarhad7@gmail.com>
2025-06-20 02:16:46 -0400

    added routes for editing movie from category route and removing movie from category route, editing category itself

--- routes/CategoryRouter.js
+++ routes/CategoryRouter.js
@@ -9,12 +9,30 @@ const CategoryRouter = Router();
 
 const CategoryController = require("../controllers/CategoryController");
 const CategoryIndexController = require("../controllers/CategoryIndexController");
-
+const CategoryDetailsController = require("../controllers/CategoryDetailsController");
+const EditMovieController = require("../controllers/EditMovieController");
+const EditPostMovieController = require("../controllers/EditPostMovieController");
+const CategoryEditController = require("../controllers/CategoryEditController");
+const CategoryEditPostController = require("../controllers/CategoryEditPostController");
 
 CategoryRouter.get("/", CategoryIndexController);
 
 
 CategoryRouter.get("/create", CategoryController);
+
+
+CategoryRouter.get("/:id", CategoryDetailsController);
+
+
+CategoryRouter.get("/cat/edit/:id", EditMovieController);
+
+CategoryRouter.post("/cat/edit/:id", EditPostMovieController);
+
+
+CategoryRouter.get("/edit/:id", CategoryEditController);
+
+CategoryRouter.post("/edit/:id", CategoryEditPostController);
+/*CategoryRouter.get("/remove/:id", CategoryRemoveController);*/
 
 
 
