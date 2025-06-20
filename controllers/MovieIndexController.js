e75b3d32f23313e04c360ac3b0d4708c0c839f5b
Farhad Rasoli <rasoolyfarhad7@gmail.com>
2025-06-20 02:13:54 -0400

    added a path property to help with redirection process

--- controllers/MovieIndexController.js
+++ controllers/MovieIndexController.js
@@ -9,7 +9,7 @@ const {getAllMovies} = require("../database/queries");
 
 const MovieIndexController = async function(req, res) {
     const movies = await getAllMovies();
-    res.render("Pages/index", {content : "../Partials/Movies", movies});
+    res.render("Pages/index", {content : "../Partials/MoviesPage", movies, path : "/movies"});
 }
 
 
