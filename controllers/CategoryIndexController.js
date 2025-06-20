8d05996d601a98ed522d9188d0788d47398ea2b1
Farhad Rasoli <rasoolyfarhad7@gmail.com>
2025-06-20 02:06:59 -0400

    NTS

--- controllers/CategoryIndexController.js
+++ controllers/CategoryIndexController.js
@@ -10,7 +10,7 @@ const {getAllCategories} = require("../database/queries");
 const CategoryIndexController = async function(req, res){
     const categories = await getAllCategories();
 
-    res.render("Pages/index", {content : "../Partials/Categories", categories});
+    res.render("Pages/index", {content : "../Partials/CategoriesPage", categories});
 }
 
 
