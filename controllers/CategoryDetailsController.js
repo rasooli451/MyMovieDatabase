a04995b80e7c78483953747ce4cee49aa60039b2
Farhad Rasoli <rasoolyfarhad7@gmail.com>
2025-06-24 00:58:08 -0400

    implemented filtering functionality

--- controllers/CategoryDetailsController.js
+++ controllers/CategoryDetailsController.js
@@ -1,6 +1,6 @@
 
 
-const {getMoviesByCategory,getCategory} = require("../database/queries");
+const {getMoviesByCategory,getCategory, filterMovies} = require("../database/queries");
 
 
 
@@ -8,8 +8,28 @@ const {getMoviesByCategory,getCategory} = require("../database/queries");
 const CategoryDetailsController = async function(req, res){
     const {id} = req.params;
     const category = await getCategory(id);
-    const movies = await getMoviesByCategory(id);
-    res.render("Pages/index", {content : "../Partials/Category", movies, category, path : `/category/cat`});
+    let movies = null;
+    if (req.query.order){
+        if (req.query.order == "highrating"){
+            movies = await filterMovies("rating", id, false);
+        }
+        else if(req.query.order == "lowrating"){
+            movies = await filterMovies("rating", id, true);
+        }
+        else if(req.query.order == "recentrelease"){
+            movies = await filterMovies("releasedate", id, false);
+        }
+        else if(req.query.order == "oldestrelease"){
+            movies = await filterMovies("releasedate", id, true);
+        }
+        else{
+            return res.render("Pages/index", {content : "../Partials/Errors", errors : [{msg : `Unknown Query, can't order by ${req.query.order}!` }]})
+        }
+    }
+    else{
+        movies = await getMoviesByCategory(id);
+    }
+    res.render("Pages/index", {content : "../Partials/Category", movies, category, path : `/category/cat`, target : `/category/${id}`, order : req.query.order});
 }
 
 
