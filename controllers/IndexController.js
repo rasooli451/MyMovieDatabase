c94e5c76d54c4ddaacee1a0d1beb00214461e7e2
Farhad Rasoli <rasoolyfarhad7@gmail.com>
2025-06-24 01:09:35 -0400

    changed from all movies to feature movies only to get just a select amount of movies to show at homepage

--- controllers/IndexController.js
+++ controllers/IndexController.js
@@ -1,10 +1,10 @@
 
-const {getAllCategories, getAllMovies} = require("../database/queries");
+const {getAllCategories, getFeaturedMovies} = require("../database/queries");
 const asyncHandler = require("express-async-handler");
 
 const IndexController = asyncHandler(async function(req, res){
     const categories = await getAllCategories();
-    const movies = await getAllMovies();
+    const movies = await getFeaturedMovies();
     res.render("Pages/index", {content : "../Partials/homePage", categories, movies, path : ""});
 })
 
