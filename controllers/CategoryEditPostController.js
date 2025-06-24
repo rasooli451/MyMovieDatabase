be9e7def57dc8f323084eb6a9d9239d4a703b9ec
Farhad Rasoli <rasoolyfarhad7@gmail.com>
2025-06-24 00:59:43 -0400

    handled redirection to the right route after category edit and took into account if user doesn't upload a new picture

--- controllers/CategoryEditPostController.js
+++ controllers/CategoryEditPostController.js
@@ -9,15 +9,20 @@ const validateImage = [
     body("name").notEmpty().withMessage("An error occured, name should not be empty!"),
     body("description").notEmpty().withMessage("An error occured, description should not be empty!"),
     body("file").custom((value, {req}) => {
-        const file = req.files.picture;
-        if (!file.mimetype.includes("image")){
-            throw new Error("File Uploaded is not an image");
+        if (req.files){
+            const file = req.files.picture;
+            if (!file.mimetype.includes("image")){
+                throw new Error("File Uploaded is not an image");
+            }
+            const tenMB = 10485760;
+            if (file.size > tenMB){
+                throw new Error("File uploaded is too large");
+            }
+            return true;
+            }
+        else{
+            return true;
         }
-        const tenMB = 10485760;
-        if (file.size > tenMB){
-            throw new Error("File uploaded is too large");
-        }
-        return true;
     })
 ]
 
@@ -30,12 +35,23 @@ const CategoryEditPostController = [validateImage, async function(req, res){
     }
     const {id} = req.params;
     const {name, description} = req.body;
-    const fileData = req.files.picture.data;
-    const dataType = req.files.picture.mimetype;
-    const result = await editCategory(id, name, description, fileData, dataType)
+    let result = false;
+    if (req.files){
+        const fileData = req.files.picture.data;
+        const dataType = req.files.picture.mimetype;
+        result = await editCategory(id, name, description, fileData, dataType);
+    }
+    else{
+         result = await editCategory(id, name, description, null, null);
+    }
 
     if (result){
-        res.redirect(`/category/${id}`);
+        if (req.originalUrl.includes("editCat"))
+              res.redirect(`/category/${id}`);
+        else if(req.originalUrl.includes("category/editc"))
+             res.redirect(`/category`);
+        else
+             res.redirect("/");
     }
     else{
         res.render("Pages/index", {content : "../Partials/Errors", errors : [{msg : "An error occured, a category by this name already exists!"}]});
