1ec42cc1cb3e034647604c906da250dca17e27eb
Farhad Rasoli <rasoolyfarhad7@gmail.com>
2025-06-24 01:12:52 -0400

    filtering functionality implemented for movies page

--- controllers/MovieIndexController.js
+++ controllers/MovieIndexController.js
@@ -1,6 +1,6 @@
 
 
-const {getAllMovies} = require("../database/queries");
+const {getAllMovies, filterMovies} = require("../database/queries");
 
 
 
@@ -8,8 +8,28 @@ const {getAllMovies} = require("../database/queries");
 
 
 const MovieIndexController = async function(req, res) {
-    const movies = await getAllMovies();
-    res.render("Pages/index", {content : "../Partials/MoviesPage", movies, path : "/movies"});
+    let movies = null;
+    if (req.query.order){
+        if (req.query.order == "highrating"){
+            movies = await filterMovies("rating", null, false);
+        }
+        else if(req.query.order == "lowrating"){
+            movies = await filterMovies("rating", null, true);
+        }
+        else if(req.query.order == "recentrelease"){
+            movies = await filterMovies("releasedate", null, false);
+        }
+        else if(req.query.order == "oldestrelease"){
+            movies = await filterMovies("releasedate", null, true);
+        }
+        else{
+            return res.render("Pages/index", {content : "../Partials/Errors", errors : [{msg : `Unknown Query, can't order by ${req.query.order}!` }]})
+        }
+    }
+    else{
+        movies = await getAllMovies();
+    }
+    res.render("Pages/index", {content : "../Partials/MoviesPage", movies, path : "/movies", target : "/movies", order : req.query.order});
 }
 
 
