732c8345febdce68e4f278940e99bdf000c92106
Farhad Rasoli <rasoolyfarhad7@gmail.com>
2025-06-24 00:58:57 -0400

    handled redirection to the right route after category edit

--- controllers/CategoryEditController.js
+++ controllers/CategoryEditController.js
@@ -11,8 +11,13 @@ const {getCategory} = require("../database/queries");
 const CategoryEditController = async function(req, res){
     const {id} = req.params;
     const category = await getCategory(id);
-
-    res.render("Pages/index", {content : "../Partials/EditCategory", category});
+    if (req.originalUrl.includes("category/editc")){
+        res.render("Pages/index", {content : "../Partials/EditCategory", category, path : "/category/editc/"});
+    }
+    else if(req.originalUrl.includes("editCat"))
+        res.render("Pages/index", {content : "../Partials/EditCategory", category, path : "/category/editCat/"});
+    else    
+        res.render("Pages/index", {content : "../Partials/EditCategory", category, path : "/editc/"});
 }
 
 
