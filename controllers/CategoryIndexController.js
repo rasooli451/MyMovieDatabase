c8766fb58c7e0167f9921d225088bd259d1790e6
Farhad Rasoli <rasoolyfarhad7@gmail.com>
2025-06-24 01:00:22 -0400

    added a path property to render function in order to facilitate redirection

--- controllers/CategoryIndexController.js
+++ controllers/CategoryIndexController.js
@@ -10,7 +10,7 @@ const {getAllCategories} = require("../database/queries");
 const CategoryIndexController = async function(req, res){
     const categories = await getAllCategories();
 
-    res.render("Pages/index", {content : "../Partials/CategoriesPage", categories});
+    res.render("Pages/index", {content : "../Partials/CategoriesPage", categories, path : "category"});
 }
 
 
