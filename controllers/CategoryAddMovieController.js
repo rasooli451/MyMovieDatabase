8b66e0838c562d8cb64c1d792bc33244d754c514
Farhad Rasoli <rasoolyfarhad7@gmail.com>
2025-06-24 00:49:42 -0400

    controller for handling adding a movie from a category page

---
+++ controllers/CategoryAddMovieController.js
@@ -0,0 +1,13 @@
+
+const {getAllCategories} = require("../database/queries");
+
+
+const CategoryAddMovieController = async function(req, res){
+    const categories = await getAllCategories();
+    const {id} = req.params;
+    res.render("Pages/index", {content : "../Partials/AddMovie", fromCategory : true, categories, id})
+}
+
+
+
+module.exports = CategoryAddMovieController;
