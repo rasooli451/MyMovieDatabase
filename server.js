18d0cac125691e688f7a2d2c49fc7d2de940fa6c
Farhad Rasoli <rasoolyfarhad7@gmail.com>
2025-06-25 21:28:11 -0400

    added error middlewares

--- server.js
+++ server.js
@@ -25,14 +25,13 @@ const MovieRouter = require("./routes/MovieRouter");
 
 
 
-//for homepage, includes the template categories
 
 app.use("/", indexRouter);
 
 
 
 
-//for 
+
 
 app.use("/category", CategoryRouter);
 
@@ -41,7 +40,16 @@ app.use("/category", CategoryRouter);
 app.use("/movies", MovieRouter)
 
 
-//
+
+
+app.use((req, res, next)=>{
+    res.status(404).render("Pages/index", {content : "../Partials/Errors", errors : [{msg : "Sorry, the page you were looking for cannot be found."}]})
+})
+
+
+app.use((err, req, res, next) =>{
+    res.status(500).render("Pages/index", {content : "../Partials/Errors", errors : [{msg : err.message}]})
+})
 
 
 
